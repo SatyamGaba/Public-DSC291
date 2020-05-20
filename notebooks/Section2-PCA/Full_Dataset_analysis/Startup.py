74ae879da7d55c4da6bc1bad59041894704ec97f
SatyamGaba <satyamgb321@gmail.com>
2020-05-20 02:59:13 -0700

    change directories and added data

--- notebooks/Section2-PCA/Full_Dataset_analysis/Startup.py
+++ notebooks/Section2-PCA/Full_Dataset_analysis/Startup.py
@@ -1,5 +1,6 @@
 import sys
-sys.path.append('/mnt/workspace/Public-DSC291/notebooks/Section2-PCA/PCA/lib/')
+# sys.path.append('/mnt/workspace/Public-DSC291/notebooks/Section2-PCA/PCA/lib/')
+sys.path.append('/mnt/d/02_acads/3.SP-20/dsc291/Public-DSC291/notebooks/Section2-PCA/PCA/lib/')
 
 import numpy as np
 from time import time
@@ -35,10 +36,14 @@ from pyspark.sql import SQLContext
 sqlContext = SQLContext(sc)
 t1=time()
 print('started SparkContext and SQLContext in %4.2f seconds'%(t1-t0))
-df = sqlContext.sql("SELECT * FROM parquet.`/tmp/weather.parquet`")
+# df = sqlContext.sql("SELECT * FROM parquet.`/tmp/weather.parquet`")
+df = sqlContext.sql("SELECT * FROM parquet.`/mnt/d/02_acads/3.SP-20/dsc291/Public-DSC291/Data/weather.parquet`")
+# df = sqlContext.sql("SELECT * FROM parquet.`s3a://sgaba291/weather.parquet/`")
 t2=time()
 print('loaded weather.parquet in %4.2f seconds'%(t2-t1))
-stations=sqlContext.sql("SELECT * FROM parquet.`/tmp/stations.parquet`")
+# stations=sqlContext.sql("SELECT * FROM parquet.`/tmp/stations.parquet`")
+stations=sqlContext.sql("SELECT * FROM parquet.`/mnt/d/02_acads/3.SP-20/dsc291/Public-DSC291/Data/stations.parquet`")
+# stations=sqlContext.sql("SELECT * FROM parquet.`s3a://sgaba291/stations.parquet/`")
 t3=time()
 print('loaded stations.parquet in %4.2f seconds'%(t3-t2))
 sqlContext.registerDataFrameAsTable(df,'weather')
